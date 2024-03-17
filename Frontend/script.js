1d445279fa5572d43e37dddf0d01b557c44b6d50
rye141200 <thecityhunterhd@gmail.com>
2024-03-17 16:23:55 +0200

    Minor enhancement for login page

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,6 +6,8 @@ const boxEl = document.querySelector(".box");
 //Tabbed element listener
 boxEl.addEventListener("click", function (e) {
   e.preventDefault();
+  const loginBoxEl = document.querySelector(".login-box");
+  //Event delegation
   if (e.target.classList.contains("tab")) {
     e.target.classList.add("active-tab");
     e.target.classList.remove("inactive-tab");
@@ -18,7 +20,7 @@ boxEl.addEventListener("click", function (e) {
     });
 
     const flag = e.target.parentElement.querySelector(".firstname");
-    //TODO
+    //*toggle login or signup form
     if (e.target.classList.contains("signup-tab") && !flag) {
       const html = `
       <div class="input-field firstname added">
@@ -43,30 +45,37 @@ boxEl.addEventListener("click", function (e) {
     </div>
       `;
       const htmlConfirmPassword = `
+      <div class="input-field confirmpwd added">
       <label class="input-field-label confirmpwd addedPWD" for="fname">Confirm password:</label>
       <input
-        class="input-field-element addedPWD"
+        class="input-field-element"
         type="password"
         id="confirm-input"
         name="fname"
         placeholder="Confirm password"
       />
+      </div>
+      <div class="input-field added">
+        <div class="input-field" id="signup-btn">
+          <button class="btn btn-login">Sign up</button>
+        </div>
+      </div>
       `;
-      const loginBoxEl = document.querySelector(".login-box");
+
+      loginBoxEl.removeChild(document.querySelector("#login-btn"));
       loginBoxEl.insertAdjacentHTML("afterbegin", html);
-      document
-        .querySelector("#password")
-        .insertAdjacentHTML("beforeend", htmlConfirmPassword);
+      loginBoxEl.insertAdjacentHTML("beforeend", htmlConfirmPassword);
     } else {
       if (flag) {
         const elementsToRemove = document.querySelectorAll(".added");
-        const elementsToRemovePwd = document.querySelectorAll(".addedPWD");
-        const boxEL = document.querySelector(".box");
-        const passwordEl = document.querySelector("#password");
-
-        elementsToRemove.forEach((element) => boxEl.removeChild(element));
-        elementsToRemovePwd.forEach((element) =>
-          passwordEl.removeChild(element)
+        elementsToRemove.forEach((element) => loginBoxEl.removeChild(element));
+        loginBoxEl.insertAdjacentHTML(
+          "beforeend",
+          `
+        <div class="input-field" id="login-btn">
+          <button class="btn btn-login">Login</button>
+      </div>
+        `
         );
       }
     }
