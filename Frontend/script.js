51ea73227d731ce42e4a20d7f93a6a4a65406359
rye141200 <thecityhunterhd@gmail.com>
2024-03-17 03:48:10 +0200

    Finished login frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -16,6 +16,59 @@ boxEl.addEventListener("click", function (e) {
         tab.classList.remove("active-tab");
       }
     });
+
+    const flag = e.target.parentElement.querySelector(".firstname");
     //TODO
+    if (e.target.classList.contains("signup-tab") && !flag) {
+      const html = `
+      <div class="input-field firstname added">
+      <label class="input-field-label" for="fname">First name:</label>
+      <input
+        class="input-field-element"
+        type="email"
+        id="firstname-input"
+        name="fname"
+        placeholder="John"
+      />
+    </div>
+    <div class="input-field added">
+      <label class="input-field-label" for="fname">Last name:</label>
+      <input
+        class="input-field-element"
+        type="text"
+        id="lastname-input"
+        name="fname"
+        placeholder="Smith"
+      />
+    </div>
+      `;
+      const htmlConfirmPassword = `
+      <label class="input-field-label confirmpwd addedPWD" for="fname">Confirm password:</label>
+      <input
+        class="input-field-element addedPWD"
+        type="password"
+        id="confirm-input"
+        name="fname"
+        placeholder="Confirm password"
+      />
+      `;
+      const loginBoxEl = document.querySelector(".login-box");
+      loginBoxEl.insertAdjacentHTML("afterbegin", html);
+      document
+        .querySelector("#password")
+        .insertAdjacentHTML("beforeend", htmlConfirmPassword);
+    } else {
+      if (flag) {
+        const elementsToRemove = document.querySelectorAll(".added");
+        const elementsToRemovePwd = document.querySelectorAll(".addedPWD");
+        const boxEL = document.querySelector(".box");
+        const passwordEl = document.querySelector("#password");
+
+        elementsToRemove.forEach((element) => boxEl.removeChild(element));
+        elementsToRemovePwd.forEach((element) =>
+          passwordEl.removeChild(element)
+        );
+      }
+    }
   }
 });
