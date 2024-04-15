4676adaeb6ab8ca83d9dc8af65ad300072d5fdc6
rye141200 <thecityhunterhd@gmail.com>
2024-04-15 02:42:14 +0200

    Merging Profile and Market Home

--- Frontend/General/General.js
+++ Frontend/General/General.js
@@ -12,10 +12,16 @@ menuEl.addEventListener("click", function () {
       leftMenuEl.forEach((leftMenu) => leftMenu.classList.add("hidden-menu"));
     })
   );
-
+  //*Home page listener
+  const homeEl = document.querySelector(".home");
+  homeEl.addEventListener("click", function () {
+    window.location.href = "/Frontend/MarketHome/market.html";
+  });
   //*Profile page listener
   const profileEl = document.querySelector(".profile");
-
+  profileEl.addEventListener("click", function () {
+    window.location.href = "/Frontend/Profile/profile.html";
+  });
   //*Wallet page listener
   //*Logout listener
   const logoutBtnEl = document.querySelector(".logout-icon");
